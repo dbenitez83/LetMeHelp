822c5ffddd8bb8563d5c984b562ac57a90e3fc9f
daniel.benitez <daniel.benitez@cabsa.es>
2021-02-10 20:03:48 +0100

    Plana de Lleida

--- data_reader.py
+++ data_reader.py
@@ -7,7 +7,6 @@ import os
 
 def pop_density_binning(return_colors=False):
 	"""Return bins and color scheme for population density"""
-	# First bin is 0, next is 0.1-1, ..., final is > 10000
 	bins = np.array([0, 1, 2, 4, 6, 8, 10, 50, 100, 200, 500, 1000, 1500, 2000, 2500, 5000, 10000])
 	cmap = plt.cm.get_cmap('Reds', len(bins)+1)
 	if not return_colors:
@@ -15,14 +14,13 @@ def pop_density_binning(return_colors=False):
 	else:
 		colors = []
 		for i in range(cmap.N):
-			rgb = cmap(i)[:3] # will return rgba, we take only first 3 so we get rgb
+			rgb = cmap(i)[:3] 
 			colors.append(matplotlib.colors.rgb2hex(rgb))
 
 		return bins, colors
 
 def risk_binning(return_colors=False):
 	"""Return bins and color scheme for relative median risk"""
-	# First bin is -10 000..-8 000, next is -8 000..-6 000, ..., final is 14 000-16 000
 	bins = np.arange(-10000,18000,2000)
 	cmap = plt.cm.get_cmap('RdBu', len(bins))
 	if not return_colors:
@@ -30,30 +28,138 @@ def risk_binning(return_colors=False):
 	else:
 		colors = []
 		for i in range(cmap.N):
-			rgb = cmap(i)[:3] # will return rgba, we take only first 3 so we get rgb
+			rgb = cmap(i)[:3] 
 			colors.append(matplotlib.colors.rgb2hex(rgb))
 
 		return bins, colors
 		
-# Get statistics from Statistics Finland portal for year 2018 keeping only the selected data columns
-url="/Volumes/Disco Externo/LetMeHelp!/municipios_ign.geojson"
+url="/Volumes/Disco Externo/municipios_ign.geojson"
 keep_columns = ['nameunit', 'codigoine', 'geometry']
 df=gpd.read_file(url)
+
+df=df.loc[(df['codigoine'].isin(["25041","25068","25048","25052","25068","25093","25099","25113" \
+	"25122","25135","25137","25158","25168","25205","25230","25248","25252"'250061',
+'250096',
+'250292',
+'251706',
+'250582',
+'250560',
+'250674',
+'250735',
+'250766',
+'250812',
+'250925',
+'250978',
+'251018',
+'251057',
+'251180',
+'251193',
+'251538',
+'251692',
+'251804',
+'252069',
+'252189',
+'252249',
+'252537',
+'252555','430213',
+'430290',
+'430461',
+'430542',
+'430614',
+'430733',
+'430862',
+'431016',
+'431055',
+'431074',
+'431418',
+'431303',
+'431397',
+'431423',
+'431439',
+'431460',
+'431476',
+'431589',
+'431592',
+'431687',
+'431726',
+'431763',
+'250557',
+'250729',
+'250851',
+'251039',
+'251044',
+'251109',
+'251142',
+'251327',
+'251410',
+'251431',
+'251522',
+'259118',
+'259059',
+'251919',
+'251924',
+'251977',
+'251945',
+'252167',
+'252192',
+'259078',
+'252234',
+'250030',
+'250273',
+'250463',
+'250501',
+'250707',
+'250740',
+'250962',
+'251095',
+'251305',
+'251459',
+'251543',
+'251575',
+'251765',
+'251811',
+'259025',
+'252252',
+'252173',
+'252385',
+'252424',
+'252445',
+'250557',
+'250729',
+'250851',
+'251039',
+'251044',
+'251109',
+'251142',
+'251327',
+'251410',
+'251431',
+'251522',
+'259118',
+'259059',
+'251919',
+'251924',
+'251977',
+'251945',
+'252167',
+'252192',
+'259078',
+'252234']))]
+
 cols = list(df)
 for col in keep_columns:
     cols.remove(col)
 data=df.drop(columns=cols)
-#data.rename(columns={'he_vakiy': 'pop2018', 'pinta_ala': 'area', 'nimi': 'name', 'hr_mtu': 'risk', 'posti_alue': 'zip'}, inplace=True)
 
 data.rename(columns={'nameunit': 'name','codigoine':'zip'}, inplace=True)
 data.loc[:, 'pop2018'] = 100
 data.loc[:, 'area'] = 100
 data.loc[:, 'risk'] = 000
 data['pop_density'] = data['pop2018']/data['area']
+avg_risk = np.nanmean(data['risk'])
 data['risk_relative'] = data['risk']-avg_risk
 
 
-# Assign alternative colors for population density
 bins, cmap = pop_density_binning()
 colors = []
 
@@ -63,20 +169,19 @@ for i, row in data.iterrows():
 
 data['fill_density'] = Series(colors, dtype='str', index=data.index)
 
-	bins, cmap = risk_binning()
-	colors = []
+bins, cmap = risk_binning()
+colors = []
 
-	for i, row in data.iterrows():
-		index = bins.searchsorted(row['risk_relative'])
-		colors.append(matplotlib.colors.rgb2hex(cmap(index)[:3]))
+for i, row in data.iterrows():
+	index = bins.searchsorted(row['risk_relative'])
+	colors.append(matplotlib.colors.rgb2hex(cmap(index)[:3]))
 
 data['fill'] = Series(colors, dtype='str', index=data.index)
 
-# Save data as GeoJSON
-# Note the driver cannot overwrite an existing file,
-# so we must remove it first
-outfile = 'map_data_ES.json'
+
+outfile = 'map_data_ES_filtered.json'
 if os.path.isfile(outfile):
 	os.remove(outfile)
 
 data.to_file(outfile, driver='GeoJSON')
+# ogr2ogr -f "GeoJSON" -lco COORDINATE_PRECISION=4 map_data_ES_filtered_reduced.json map_data_ES_filtered.json
